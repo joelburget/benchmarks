218dd930c8e35ed13753a3ad662e057aaaf03e30
Joel Burget <joelburget@gmail.com>
2010-06-23 16:09:45 -0400

    added homepage

--- views.py
+++ views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render_to_response
 
 def homepage(request):
 	featured_posts = Post.objects.filter(sticky=True)
-	latest_posts = Post.objects.all()[:5]
+	latest_posts = Post.objects.filter(sticky=False).order_by('published')[:5]
 	return render_to_response('homepage.html', 
 		{'featured_posts': featured_posts,
 		 'latest_posts': latest_posts})
