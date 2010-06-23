fae982f592177d7d6d15eb6d28b66d629612bcc1
Colin Drake <colin.f.drake@gmail.com>
2010-06-23 18:50:52 -0400

    Added searching, filtering, and sorting to admin.  Also, sticky attribute is editable in place from the admin form.

--- posts/admin.py
+++ posts/admin.py
@@ -1,4 +1,16 @@
 from benchmarks.posts.models import Post
 from django.contrib import admin
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+  # Order by newest on top
+  ordering = ('-published', )
+
+  # Display multiple fields in the list view
+  list_display = ('title', 'body', 'sticky')
+  list_editable = ['sticky']
+
+  # Filter and search capabilites
+  list_filter = ['published']
+  search_fields = ['title']
+
+admin.site.register(Post, PostAdmin)
