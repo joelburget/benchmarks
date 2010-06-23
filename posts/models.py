29f7a84350ff0c16928bc3ff43fef23a97d00c0d
Colin Drake <colin.f.drake@gmail.com>
2010-06-23 15:41:38 -0400

    Updated model and db to include sticky field.

--- posts/models.py
+++ posts/models.py
@@ -1,15 +1,10 @@
 from django.db import models
 
-#we can add more later
-#we may want a user and a group model
-#not sure if we can use what django provides us...
-#we probably don't even want to use slugs because we can just use the primary key
-
 class Post(models.Model):
   title = models.CharField(max_length=200)
-  slug = models.SlugField('Slug')
   published = models.DateTimeField('Date Published', auto_now_add=True)
   body = models.TextField()
+  sticky = models.BooleanField('Show on Frontpage?', default=False)
 
   def __unicode__(self):
     return self.title
