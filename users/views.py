8e47b26048a6b42df3f35b73691e57336e02ab3f
Colin Drake <colin.f.drake@gmail.com>
2010-06-24 15:45:13 -0400

    Added profile edit link, pages check to see whether you are editing your own profile or not.  Still need to add the form.

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404, render_to_response
+from django.views.generic.simple import direct_to_template
 from django.template import RequestContext
 from django.contrib.auth.models import User
 from django.contrib.comments.models import Comment
@@ -13,3 +14,17 @@ def showuser(request, uname):
                                               'posts' : posts,
                                               'comments_list' : comments
                                              }))
+
+def edituser(request, uname):
+  # Get the users wanted to edit, and the current user
+  wanteduser = get_object_or_404(User, username=uname)
+  me = request.user
+
+  # Ensure that the user editing this profile is allowed (i.e. the
+  # user and the profile are the same person)
+  if wanteduser.username == me.username:
+    # Allow edits
+    return direct_to_template(request, 'users/edituser.html')
+  else:
+    # Disallow edits
+    return direct_to_template(request, 'users/edituser_bad.html')
