3d7a3d033801f615c25033b05b8738f3775368d3
Colin Drake <colin.f.drake@gmail.com>
2010-06-24 17:20:30 -0400

    Fixed username field problem and added banner on homepage when logged out.

--- users/models.py
+++ users/models.py
@@ -5,4 +5,4 @@ from django.contrib.auth.models import User
 class UserForm(ModelForm):
   class Meta():
     model = User
-    fields = ('username', 'first_name', 'last_name', 'email')
+    fields = ('first_name', 'last_name', 'email')
