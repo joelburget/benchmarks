f4b43a74483ec6d418b6e39dc05b432e0189ecc8
Joel Burget <joelburget@gmail.com>
2010-06-24 18:09:20 -0400

    added xss protection

--- settings.py
+++ settings.py
@@ -99,7 +99,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-	'django.contrib.comments',
+    'django.contrib.comments',
     'django.contrib.admin',
-    'benchmarks.posts'
+    'benchmarks.posts',
+    'benchmarks.comment-sanitizer'
 )
