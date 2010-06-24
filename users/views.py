2440268063ba1e85b541da119fd3e1ed65eb1b87
Colin Drake <colin.f.drake@gmail.com>
2010-06-24 12:43:37 -0400

    User profiles now show comment lists.

--- users/views.py
+++ users/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 from django.contrib.auth.models import User
+from django.contrib.comments.models import Comment
 
 def showuser(request, uname):
   u = get_object_or_404(User, username=uname)
   posts = u.post_set.all()
-  return render_to_response('users/showuser.html', context_instance=RequestContext(request, {'object':u, 'posts':posts}))
+  comments = Comment.objects.filter(user=u)
+  return render_to_response('users/showuser.html', 
+    context_instance=RequestContext(request, {
+                                              'object' : u, 
+                                              'posts' : posts,
+                                              'comments_list' : comments
+                                             }))
