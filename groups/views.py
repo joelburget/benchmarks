be4deac3147785cf0de1d14b5c8f8768b9d78178
Colin Drake <colin.f.drake@gmail.com>
2010-06-27 18:34:34 -0400

    Added leave/join link to groups link.

--- groups/views.py
+++ groups/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import get_object_or_404, get_list_or_404, render_to_response
 from django.contrib.auth.models import Group
+from django.template import RequestContext
 
 def index(request):
   groups = get_list_or_404(Group)
-  return render_to_response('groups/index.html', { 'groups' : groups })
+  return render_to_response('groups/index.html', { 'groups' : groups }, context_instance=RequestContext(request))
 
 def detail(request, group_id):
   group = get_object_or_404(Group, pk=group_id)
-  return render_to_response('groups/detail.html', { 'group' : group })
+  return render_to_response('groups/detail.html', { 'group' : group }, context_instance=RequestContext(request))
