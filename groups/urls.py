88cf4391645bae37e7c469651deeb9202876e00a
Colin Drake <colin.f.drake@gmail.com>
2010-06-27 19:09:14 -0400

    Join/Leave groups now working, and added Groups link to top of the page.

--- groups/urls.py
+++ groups/urls.py
@@ -4,4 +4,6 @@ from django.contrib.auth.models import Group
 urlpatterns = patterns('',
   (r'^$', 'benchmarks.groups.views.index'),
   (r'^(?P<group_id>\d+)/$', 'benchmarks.groups.views.detail'),
+  (r'^join/$', 'benchmarks.groups.views.join'),
+  (r'^leave/$', 'benchmarks.groups.views.leave'),
 )
