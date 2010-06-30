297d345a98c6b18412ec298dd99e4dd47b795bd7
Colin Drake <colin.f.drake@gmail.com>
2010-06-29 21:07:06 -0400

    Fixed server side validation & added client side validation.

--- users/models.py
+++ users/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django import forms
 from django.forms import ModelForm
 from django.contrib.auth.models import User
 
@@ -6,3 +7,7 @@ class UserForm(ModelForm):
   class Meta():
     model = User
     fields = ('first_name', 'last_name', 'email')
+
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class':'required', 'size':'30'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class':'required', 'size':'30'}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={'class':'required email'}))
