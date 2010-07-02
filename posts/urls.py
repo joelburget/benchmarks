b006954d20c7370303d484d74cdec66341ea0403
Colin Drake <colin.f.drake@gmail.com>
2010-07-01 22:00:12 -0400

    Changed posts url.

--- posts/urls.py
+++ posts/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
   # Index
   # NOTE: Just using posts here, because / redirects to post urls
   # Not ideally in the right location, but it works
-  (r'^posts/$', 'django.views.generic.list_detail.object_list', dict(info_dict)),
+  (r'^$', 'django.views.generic.list_detail.object_list', dict(info_dict)),
 
   # View posts
   (r'^(?P<object_id>\d+)/$', 'django.views.generic.list_detail.object_detail', dict(info_dict)),
