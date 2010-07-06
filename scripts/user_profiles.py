c41be5ed2c57b0c1079ec0001cae5b6f79516abc
Colin Drake <colin.f.drake@gmail.com>
2010-07-05 18:53:25 -0700

    quick typo fix

--- scripts/user_profiles.py
+++ scripts/user_profiles.py
@@ -1,6 +1,6 @@
 # Migration script to create profiles for all users that don't have them.
 #
-# Run mange.py shell (NOT python.exe)
+# Run manage.py shell (NOT python.exe)
 # >> from scripts import user_profiles
 # >> user_profile.run()
 from django.contrib.auth.models import User
