12e6f0e5b82216ee90e2b18432dd8aacf2be7160
Colin Drake <colin.f.drake@gmail.com>
2010-07-17 16:26:08 -0400

    Fixed login/logout problems in middleware.

--- middleware/tracking.py
+++ middleware/tracking.py
@@ -1,12 +1,25 @@
+import re
 from django.conf import settings
 
 class LastPage:
+  # Tuple of regular expressions
+  # The LastPage middleware will skip tracking these.
+  __skip = (
+    r'^/dirlist.*$',
+    r'^/favicon.ico$',
+    r'^/' + settings.MEDIA_URL + '.*$',
+  )
+
   def process_request(self, request):
     path = request.get_full_path()
 
     # Check for requests we don't want to check
-    if not (path.find(settings.MEDIA_URL) == 0 or path == '/favicon.ico'):
+    match = False
 
+    for url in self.__skip:
+      match = match or re.search(url, path)
+
+    if not match:
       # Save last visited page
       if 'currentpage' in request.session:
         request.session['lastpage'] = request.session['currentpage']
