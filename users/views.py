f561cf8f08d7b91648ca7d53c27a2afe3f345ac5
Colin Drake <colin.f.drake@gmail.com>
2010-07-21 22:51:48 -0400

    User profile update.

--- users/views.py
+++ users/views.py
@@ -38,8 +38,8 @@ def index(request):
 
 def showuser(request, uname):
   u = get_object_or_404(User, username=uname)
-  posts = u.post_set.all()
-  comments = Comment.objects.filter(user=u).order_by('-submit_date')[:10]
+  posts = u.post_set.all()[:5]
+  comments = Comment.objects.filter(user=u).order_by('-submit_date')[:5]
   return render_to_response('users/showuser.html', 
     context_instance=RequestContext(request, {
                                               'object' : u, 
