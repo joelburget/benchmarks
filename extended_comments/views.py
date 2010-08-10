103d3e877559f5481f07c83d6489ac12df419119
Joel Burget <joelburget@gmail.com>
2010-08-10 13:11:03 -0400

    ha it kind of works

--- extended_comments/views.py
+++ extended_comments/views.py
@@ -9,72 +9,25 @@ from django.http import HttpResponseRedirect
 from django.core.exceptions import ObjectDoesNotExist, ValidationError
 from benchmarks.posts.models import Post
 from django.views.decorators.http import require_POST
+import datetime
+from django.http import HttpResponseNotFound
+from django.contrib.contenttypes.models import ContentType
+from django.conf import settings
 
 #This sanitizes the input the user will see in the preview area for comments
 #because that is not covered by the sanitization in comment-sanitizer/__init__.py
 #(We want them to see what will actually show up)
 @require_POST
 def post(request):
-  print request.POST
-  if request.FILES:
-    form = ExtendedCommentForm(request.POST, request.FILES)
-    if form.is_valid():
-      handle_uploaded_file(request.FILES['file'])
-  preview = "preview" in request.POST
-  #If the user is requesting a preview we must sanitize the input
-  #The other option is if they are actually posting in which case
-  #the input will already be sanitized
-  if preview:
-    comment = request.POST.__getitem__('comment')
-    p = html5lib.HTMLParser(tokenizer=sanitizer.HTMLSanitizer)
-    comment = p.parse(comment).childNodes[0].childNodes[1].toxml()[6:-7]
-	
-	#reqest.POST is immutable so we must create a new request to change request.POST
-    req = HttpRequest()
-    req.path = request.path
-    req.method = request.method
-    req.encoding = request.encoding
-    req.user = request.user
-    req.session = request.session
-    req.raw_post_data = request.raw_post_data
-    #req.urlconf = request.urlconf
-    req.FILES = request.FILES
-    req.GET = request.GET
-    req.COOKIES = request.COOKIES
-    req.META = request.META
-    req.POST = request.POST.copy()
-    req.POST.__setitem__('comment', comment)
-    
-    #return post_comment(req) 
-    return comment_posted(request)
-  else:
-    data = request.POST.copy()
-    object_pk = data.get("object_pk")
-    if object_pk is None:
-      #Ideally this should be replaced
-      return HttpResponseNotFound("<h1>There was an error with your comment, please try again</h1>")
-    try:
-      target = Post.objects.get(pk=object_pk)
-    except (ObjectDoesNotExist, ValueError, ValidationError):
-      return HttpResponseNotFound("<h1>There was an error with your comment, please try again</h1>")
-
-    form = ExtendedCommentForm(target, data=data)
-    print "\n\nform incoming!!\n\n\n"
-    print form
-    if form.security_errors():
-      return HttpResponseNotFound("<h1>There was an error with your comment, please try again</h1>")
-    if form.is_valid():
-      print "valid"
-      form.save()
-    #return post_comment(request)
-    return comment_posted(request)
-
-@require_POST
-def post(request):
   if not request.user.is_authenticated():
     return HttpResponseRedirect('/')
   else:
     data = request.POST.copy()
+    if not data.get('name', ''):
+      data['name'] = request.user.get_full_name() or request.user.username
+    if not data.get('email', ''):
+      data['email'] = request.user.email
+
     object_pk = data.get("object_pk")
     if object_pk is None:
       #Ideally this should be replaced
@@ -85,6 +38,7 @@ def post(request):
       return HttpResponseNotFound("<h1>There was an error with your comment, please try again</h1>")
 
     preview = "preview" in data
+    data.user = request.user
     
     if request.FILES:
       form = ExtendedCommentForm(target, request.FILES, data=data)
@@ -92,15 +46,19 @@ def post(request):
         handle_uploaded_file(request.FILES['file'])
     else:
       form = ExtendedCommentForm(target, data=data)
-    print "\n\nform incoming!!\n\n\n"
-    print form
     if form.security_errors():
       return HttpResponseNotFound("<h1>There was an error with your comment, please try again</h1>")
     if form.is_valid():
-      print "valid"
-      form.save()
+      comment = ExtendedComment(content_type = ContentType.objects.get(app_label='posts', model='post'),
+                                object_pk = object_pk,
+                                site_id = settings.SITE_ID,
+                                user = request.user,
+                                comment = form.cleaned_data["comment"],
+                                published = datetime.datetime.now())
+      comment.save()
+    else:
+      print form.errors
     return comment_posted(request)
-
 
 def handle_uploaded_file(f):
   path = os.path.join(MEDIA_ROOT, 'uploads/') + f.name
