00e1b545543e1428b01e970fedca963f5c30de2e
Colin Drake <colin.f.drake@gmail.com>
2010-08-11 22:55:13 -0400

    Sanity check/bulletproofing of user creation/updating. Ensures we don't get group related errors.

--- users/models.py
+++ users/models.py
@@ -1,16 +1,38 @@
-from django.db import models
 from django import forms
-from django.forms import ModelForm
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
+from django.db import models
 from django.db.models.signals import post_save
+from django.forms import ModelForm
 
+# Assign users to groups by default, makes code 100% bulletproof
+def join_groups_on_create(sender, instance, created, **kwargs):
+  # Check to see if we have a generic Users group
+  g = Group.objects.get(name='Users')
+  if not g:
+   g = Group(name='Users')
+   g.save()
+
+  # Check to ensure we're making a new record, not updating one
+  if created:
+      instance.groups = [g]
+      instance.save()
+  else:
+    # Updating record, check to see if user has left all groups
+    if instance.groups.all() == []:
+      # Ensure users are always at least a part of one group
+      instance.groups = [g]
+      instance.save()
+      
+post_save.connect(join_groups_on_create, sender=User)
+
+# User Profiles
 class UserProfile(models.Model):
   user = models.ForeignKey(User, unique=True)
   bio = models.CharField(max_length=500)
   showemail = models.BooleanField(default=True)
 
-  #personalized rss subscriptions
-  #this may not be the right place to put these
+  # Personalized rss subscriptions
+  # This may not be the right place to put these
   commentResponseSubscribe = models.BooleanField(default=True)
   ownPostCommentSubscribe = models.BooleanField(default=True)
   groupPostSubscribe = models.BooleanField(default=True)
@@ -29,12 +51,14 @@ class UserProfile(models.Model):
   def __unicode__(self):
     return 'Profile for %s' % (self.user.username,)
 
+# Set up profiles for new users
 def new_profile(sender, instance, created, **kwargs):
   if created:
     profile, created = UserProfile.objects.get_or_create(user=instance)
 
 post_save.connect(new_profile, sender=User)
 
+# Form for user objects
 class UserForm(ModelForm):
   class Meta():
     model = User
