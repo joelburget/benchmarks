cfe729b5b8ae7ee49c3873821bd075adde40038b
Joel Burget <joelburget@gmail.com>
2010-09-04 16:09:33 -0400

    Fix bug in TeX implementation.

--- templatetags/templatetags/tex.py
+++ templatetags/templatetags/tex.py
@@ -1,5 +1,6 @@
 import re
 import urllib
+
 from django import template
 from django.utils.safestring import mark_safe
 from django.template.defaultfilters import stringfilter
@@ -8,11 +9,23 @@ register = template.Library()
 
 def __replace(m):
   CHARTS_URL = 'http://chart.apis.google.com/chart?cht=tx&chl='
-  return '<img src="%s" alt="%s" />' % (CHARTS_URL + urllib.quote_plus(m.group(1)), m.group(1))
+  #return '<img src="%s" alt="%s" />' % (CHARTS_URL + urllib.quote_plus(m.group(1)), m.group(1))
+  return '<img src="%s" alt="%s" />' % (CHARTS_URL + urllib.quote(m.group(1)), m.group(1))
 
 @register.filter
 @stringfilter
 def tex_to_images(value):
+  """Filter to replace a formula with a TeX image
+
+  Usage:
+  {{ "$$x^2$$"|tex_to_images }}
+
+  Note:
+  The image is downloaded from Google's Chart API.
+  See http://code.google.com/apis/chart/docs/gallery/formulas.html
+
+  """
+
   svalue = re.sub(
             '\$\$([^$]*)\$\$',
             __replace,
