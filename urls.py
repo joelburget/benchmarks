320c8a466a54db8d866947060b27404de0120b27
Colin Drake <colin.f.drake@gmail.com>
2010-09-14 10:45:51 -0400

    Add password reset capability

--- urls.py
+++ urls.py
@@ -31,6 +31,12 @@ urlpatterns = patterns('',
   (r'^rss/$', 'benchmarks.views.rss'),
   (r'^atom/$', 'benchmarks.views.atom'),
 
+  # Password reset
+  (r'^password_reset/$', 'django.contrib.auth.views.password_reset'),
+  (r'^password_reset/done/$', 'django.contrib.auth.views.password_reset_done'),
+  (r'^reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm'),
+  (r'^reset/done/$', 'django.contrib.auth.views.password_reset_complete'),
+
   # Login/logout
   (r'^login/$', 'benchmarks.views.loginuser'),
   (r'^logout/$', 'benchmarks.views.logoutuser'),
